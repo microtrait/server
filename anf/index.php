7c6d720e7c0eb89589fe1a2ed4eee139375bbb7d
i <ixteam@x-team.com>
2019-05-20 16:44:19 -0400

    COnnection test

--- anf/index.php
+++ anf/index.php
@@ -1,4 +1,22 @@
 <?php
+require dirname(__DIR__). '\plugins\redis\src\Autoloader.php';
+Predis\Autoloader::register();
+$args = [
+    'scheme' => 'tcp',
+    'host'   => 'microtraitdb.redis.cache.windows.net',
+    'port'   => 6380,
+];
+$options = [
+    'parameters' => [
+        'password' => 'OPl6NTSBkG5H29bpN2xJBbNhgakRDBb4kMQc+2VE7CQ=',
+        'ssl' => 'True',
+        'abortConnect'=>'False',
+    ]
+];
+//'ssl=True,abortConnect=False'
+$client = new Predis\Client($args,$options);
+
+$client->set('foo','bae');
 if(!isset($_GET['id'])){
     echo 'ID not found';
     die;
